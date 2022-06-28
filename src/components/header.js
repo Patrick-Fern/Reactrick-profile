55e04931d2904fb682a4f8055a469b0496ec130e
patfern <patrick.j.fernbach@gmail.com>
2022-06-27 21:45:03 -0400

    blog to resume

--- src/components/header.js
+++ src/components/header.js
@@ -34,7 +34,7 @@ function Header({ currentPage, handlePageChange })  {
                     // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
                     className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
                     >
-                    Blog
+                    Resume
                     </a>
                 </li>
                 <li className="nav-item">
